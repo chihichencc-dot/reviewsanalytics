15d7876590abc9a08ccc66d80385c0d260a5a37a
CC <chihichen.cc@gmail.com>
2025-10-20 13:49:36 -0700

    reviews len average calculation read.py

--- read2.py
+++ read2.py
@@ -4,10 +4,17 @@ with open ('reviews.txt', 'r') as f:
 	for line in f:
 		data.append(line)
 		count += 1
-		if count % 1000 == 0:
-			print(len(data))
+		#if count % 1000 == 0:
+			#print(len(data))
 
-print(len(data))
+print('finish file reading, and there is', len(data), 'data in total' )
+
+sum_len = 0
+for d in data:
+	sum_len += len(d)
+	
+
+len_avg = sum_len/len(data)
+print('the average length of the review is:', len_avg)
+
 print(data[0])
-print('---------------------------------')
-print(data[1])
